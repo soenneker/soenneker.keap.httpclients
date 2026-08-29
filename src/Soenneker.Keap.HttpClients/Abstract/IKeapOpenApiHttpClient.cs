2443d1670728f9c56837e15576a17ff1cb38356f
soenneker <jake@soenneker.com>
2026-08-29 14:17:08 -0500

    Improve public API XML documentation

--- src/Soenneker.Keap.HttpClients/Abstract/IKeapOpenApiHttpClient.cs
+++ src/Soenneker.Keap.HttpClients/Abstract/IKeapOpenApiHttpClient.cs
@@ -2,7 +2,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.Keap.HttpClients.Abstract;
 
 /// <summary>
@@ -10,5 +9,10 @@ namespace Soenneker.Keap.HttpClients.Abstract;
 /// </summary>
 public interface IKeapOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Keap OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
