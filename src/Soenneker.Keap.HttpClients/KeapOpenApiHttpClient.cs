1bb0d8f78ad807b104c0c20a955856be52fe22cc
soenneker <jake@soenneker.com>
2026-09-03 22:01:26 -0500

    Add interface inheritdoc references

--- src/Soenneker.Keap.HttpClients/KeapOpenApiHttpClient.cs
+++ src/Soenneker.Keap.HttpClients/KeapOpenApiHttpClient.cs
@@ -11,6 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Keap.HttpClients;
 
+/// <inheritdoc cref="IKeapOpenApiHttpClient" />
 public sealed class KeapOpenApiHttpClient : IKeapOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
